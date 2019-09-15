9240f219ad8138ca5d06327a84a493aa097eceda
sumanth0309 <55318263+sumanth0309@users.noreply.github.com>
2019-09-15 21:05:40 +0530

    Update WordCount.py

--- scripts/WordCount.py
+++ scripts/WordCount.py
@@ -9,7 +9,7 @@ def compiledWords(text):
 conf = SparkConf().setMaster("local").setAppName("Word Count")
 sc = SparkContext(conf = conf)
 
-linesRdd = sc.textFile("H:\Practice\BigData-DataFiles\Book.txt")
+linesRdd = sc.textFile("file:///data-files/Book.txt")
 wordsRdd = linesRdd.flatMap(compiledWords)
 wordCountSortedRdd = wordsRdd.map(lambda x: (x,1)).reduceByKey(lambda x,y: x+y).map(lambda x: (x[1],x[0])).sortByKey()
 results = wordCountSortedRdd.collect()
